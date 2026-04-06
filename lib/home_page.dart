6e73f2711c4f92cbbe044c83ec1a3ff05bf5abf6
minhajulIslam4553 <minhajul.cse.00724105101045@aust.edu>
2026-04-06 23:12:38 +0600

    home service account merged

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'location_page.dart';
+import 'service.dart';
+import 'account.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,173 +13,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int selectedIndex = 0;
 
+  List<Widget> pages = [
+    const HomeContent(),
+    const ServicePage(),
+    const AccountPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade100,
 
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              const Row(
-                children: [
-                  Icon(Icons.local_hospital, color: Colors.red),
-                  SizedBox(width: 6),
-                  Text(
-                    "Flash Aid",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20),
-
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(18),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(160),
-                ),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Emergency Ambulance Service",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 6),
-                    Text(
-                      "Fast and reliable medical transport.",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 20),
-
-              const Text(
-                "Services",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-              const SizedBox(height: 12),
-
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  children: [
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "Ambulance",
-                        imagePath: "assets/ambulance.png",
-                      ),
-                    ),
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "ICU",
-                        imagePath: "assets/icu.png",
-                      ),
-                    ),
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "Freezer",
-                        imagePath: "assets/freezer.png",
-                      ),
-                    ),
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "Air Ambulance",
-                        imagePath: "assets/air.png",
-                      ),
-                    ),
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "CCU",
-                        imagePath: "assets/ccu.png",
-                      ),
-                    ),
-
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LocationPage()),
-                        );
-                      },
-                      child: const ServiceCard(
-                        title: "Emergency Service",
-                        imagePath: "assets/advanced.png",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      body: pages[selectedIndex],
 
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: selectedIndex,
@@ -196,6 +43,176 @@ class _HomePageState extends State<HomePage> {
           BottomNavigationBarItem(
               icon: Icon(Icons.person), label: "Account"),
         ],
+      ),
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  const HomeContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            const Row(
+              children: [
+                Icon(Icons.local_hospital, color: Colors.red),
+                SizedBox(width: 6),
+                Text(
+                  "Flash Aid",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(18),
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(160),
+              ),
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Emergency Ambulance Service",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    "Fast and reliable medical transport.",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            const Text(
+              "Services",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: [
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "Ambulance",
+                      imagePath: "assets/ambulance.png",
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "ICU",
+                      imagePath: "assets/icu.png",
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "Freezer",
+                      imagePath: "assets/freezer.png",
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "Air Ambulance",
+                      imagePath: "assets/air.png",
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "CCU",
+                      imagePath: "assets/ccu.png",
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LocationPage()),
+                      );
+                    },
+                    child: const ServiceCard(
+                      title: "Emergency Service",
+                      imagePath: "assets/advanced.png",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
